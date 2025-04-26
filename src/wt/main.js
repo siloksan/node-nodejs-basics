5d53c100f961765329d7fea6b7041c3460da8daa
siloksan <tutel07@gmail.com>
2025-04-26 16:32:32 +0300

    refactor: worker function

--- src/wt/main.js
+++ src/wt/main.js
@@ -10,27 +10,27 @@ const performCalculations = async () => {
 	const promises = [];
 
 	for (let i = 0; i < numberOfThreads; i++) {
-		promises.push(
-			new Promise((resolve) => {
-				const worker = new Worker(filePath, {
-					workerData: initialNumber + i,
+		const promise = new Promise((resolve) => {
+			const worker = new Worker(filePath, {
+				workerData: initialNumber + i,
+			});
+			worker.on('message', (data) => {
+				resolve({
+					status: 'resolved',
+					data,
 				});
-				worker.on('message', (data) => {
-					resolve({
-						status: 'resolved',
-						data,
-					});
-				});
-				worker.on('error', () => {
+			});
+			worker.on('error', () => {
+				resolve({ status: 'error', data: null });
+			});
+			worker.on('exit', (code) => {
+				if (code !== 0 && !promises[i]) {
 					resolve({ status: 'error', data: null });
-				});
-				worker.on('exit', (code) => {
-					if (code !== 0 && !results[index]) {
-						resolve({ status: 'error', data: null });
-					}
-				});
-			})
-		);
+				}
+			});
+		});
+
+		promises.push(promise);
 	}
 
 	console.log(await Promise.all(promises));
